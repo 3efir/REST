621141d9dceff77a09492a42322154fb4d04d954
3efir <donka91@mail.ru>
2015-05-15 01:41:07 +0300

    112312312

---
+++ server/api/shop/models/utilities/EncoderModel.php
@@ -0,0 +1,49 @@
+<?php
+class EncoderModel
+{
+	private static $instance = null;
+	private function __construct()
+    {}
+	// singleton
+    static public function getInstance()
+    {
+        if (is_null(self::$instance))
+        {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+	public function encode($type, $data)
+	{
+		switch($type)
+		{
+			case 'json':
+			return $this -> json($data);
+			break;
+			case 'xml':
+			return $this -> xml($data);
+			break;
+		}
+	}
+	public function json($data)
+	{
+		return json_encode($data);
+	}
+	// incoming param: password
+// return hash password
+	public function getHashPass($pass)
+	{
+		return password_hash($pass, PASSWORD_DEFAULT);
+	}
+// incoming params: hash and pass
+// return true if hash = pass or false
+	public function validPass($hash, $pass)
+	{
+		return password_verify($pass, $hash);
+	}
+	public function createToken($data)
+	{
+		return md5($data);
+	}
+}
+?>
